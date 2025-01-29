062c74c938c9df32f486ff4ff8fcd233d8fc1b33
bastien <bastien.raoult1pro@gmail.com>
2025-01-29 17:03:48 +0100

    Enhance footer design with logo placement and updated styling [#20]

--- src/components/organisms/Footer/Footer.tsx
+++ src/components/organisms/Footer/Footer.tsx
@@ -2,13 +2,14 @@ import Typography from "@/components/atoms/Typography/Typography";
 
 export default function Footer() {
     return (
-        <footer className="bg-gray-800 text-white py-4">
-            <div className="container mx-auto px-4">
-                <Typography variant="p" underline>
-                    © 2025 - Alexandre Goumain & Bastien Raoult - Tout droit
-                    réservé.
-                </Typography>
-            </div>
+        <footer className="relative mt-20 bg-gray-900 px-4 pt-20">
+            <div className="absolute -top-10 left-1/2 h-16 w-16 -translate-x-1/2 rounded-xl border-4 border-sky-500 bg-white p-2"><img className="h-full object-contain" src="/ReactShop_logo.webp" alt="" /></div>
+            <Typography variant="p" underline className="py-10 text-center[]">
+                © 2025 - Alexandre Goumain & Bastien Raoult - Tout droit
+                réservé.
+            </Typography>
         </footer>
+
+
     );
 }
