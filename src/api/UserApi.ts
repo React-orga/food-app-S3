829d4cea9c87f59bf10e1e44f79b92beb647195f
Alexandre GOUMAIN <alexandre26goumain@gmail.com>
2025-01-30 11:32:45 +0100

    Fix api call

--- src/api/UserApi.ts
+++ src/api/UserApi.ts
@@ -1,13 +1,14 @@
 import { IUserApiResponse } from "@/types/UserApi/UserApiProps";
 import axios from "axios";
-import { BASE_URL } from "./ProductApi";
+
+const BASE_URL = "https://fakestoreapi.com";
 
 export const fetchUsers = async (): Promise<IUserApiResponse> => {
-    const response = await axios.get(`${BASE_URL}users`);
+    const response = await axios.get(`${BASE_URL}/users`);
     return response.data;
 };
 
 export const fetchUserById = async (id: number): Promise<IUserApiResponse> => {
-    const response = await axios.get(`${BASE_URL}users/${id}`);
+    const response = await axios.get(`${BASE_URL}/users/${id}`);
     return response.data;
 };
