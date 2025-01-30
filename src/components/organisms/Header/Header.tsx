d0033facd7a1fc75ba6e09daefc61f691c38dcf6
bastien <bastien.raoult1pro@gmail.com>
2025-01-30 09:59:35 +0100

    Refactor API response types for product fetching and update Home component mapping

--- src/components/organisms/Header/Header.tsx
+++ src/components/organisms/Header/Header.tsx
@@ -12,7 +12,7 @@ export default function Header({ links }: HeaderProps) {
                         <span>React Shop</span>
                     </a>
                     <input className="peer hidden" type="checkbox" id="navbar-open" />
-                    <label className="absolute right-0 mt-1 cursor-pointer text-xl sm:hidden" for="navbar-open">
+                    <label className="absolute right-0 mt-1 cursor-pointer text-xl sm:hidden" htmlFor="navbar-open">
                         <span className="sr-only">Toggle Navigation</span>
                         <svg xmlns="http://www.w3.org/2000/svg" width="0.88em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 448 512"><path fill="currentColor" d="M0 96c0-17.7 14.3-32 32-32h384c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zm0 160c0-17.7 14.3-32 32-32h384c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zm448 160c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32h384c17.7 0 32 14.3 32 32z" /></svg>
                     </label>
